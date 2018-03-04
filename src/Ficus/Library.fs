97f55e4f3c2302b1ab903a103278a74683a827be
Robin Munn <rmunn@pobox.com>
2018-03-04 22:46:06 +0700

    How to count bits efficiently without popcnt
    If you don't have access to the popcnt instruction, this is the fastest
    way to count the number of set bits in a 32-bit number.

--- src/Ficus/Library.fs
+++ src/Ficus/Library.fs
@@ -1,5 +1,9 @@
 namespace Ficus
 
-module Say =
-    let hello name =
-        sprintf "Hello %s" name
+module BitUtils =
+    // https://gist.github.com/rmunn/bc49d32a586cdfa5bcab1c3e7b45d7ac
+    let bitcount n =
+        let count2 = n - ((n >>> 1) &&& 0x55555555)
+        let count4 = (count2 &&& 0x33333333) + ((count2 >>> 2) &&& 0x33333333)
+        let count8 = (count4 + (count4 >>> 4)) &&& 0x0f0f0f0f
+        (count8 * 0x01010101) >>> 24
--- tests/Ficus.Tests/Tests.fs
+++ tests/Ficus.Tests/Tests.fs
@@ -7,7 +7,9 @@ open Ficus
 [<Tests>]
 let tests =
   testList "samples" [
-    testCase "Say hello all" <| fun _ ->
-      let subject = Say.hello "all"
-      Expect.equal subject "Hello all" "You didn't say hello"
+    testCase "A few sample counts" <| fun _ ->
+      Expect.equal (BitUtils.bitcount 0) 0 "0 had wrong bitcount"
+      Expect.equal (BitUtils.bitcount -1) 32 "-1 had wrong bitcount"
+      Expect.equal (BitUtils.bitcount 8) 1 "8 had wrong bitcount"
+      Expect.equal (BitUtils.bitcount 7) 3 "7 had wrong bitcount"
   ]
